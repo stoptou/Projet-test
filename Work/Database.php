6ec00bb8b3dc9327a6865aa19c75a13d48d6d361
Eric_Caujolle <ericcaujollebert@gmail.com>
2022-04-14 20:33:29 +0200

    tentative modif fichier ignoré

--- Work/Database.php
+++ Work/Database.php
@@ -29,7 +29,7 @@ class Database
         {
             if(self::$pdo===null)
             {
-                $pdo= new PDO('mysql:host=localhost:3306;dbname=abi','root', '');
+                $pdo= new PDO('mysql:host=localhost:3306;dbname=abi','root2', 'totomegalo');
                 $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
                 self::$pdo=$pdo;
             }
